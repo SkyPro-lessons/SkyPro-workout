64532fd6495189e29f21cc28ea89f1a25f134421
Telion <www.neon@mail.ru>
2023-02-07 19:23:02 +0500

    Инициализация приложения

---
+++ src/main/java/site/telion/skyproworkout/SkyProWorkoutApplication.java
@@ -0,0 +1,13 @@
+package site.telion.skyproworkout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyProWorkoutApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkyProWorkoutApplication.class, args);
+    }
+
+}
---
+++ src/test/java/site/telion/skyproworkout/SkyProWorkoutApplicationTests.java
@@ -0,0 +1,13 @@
+package site.telion.skyproworkout;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkyProWorkoutApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
