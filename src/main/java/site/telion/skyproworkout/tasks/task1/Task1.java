ff1104ec3dda4977ffdd5313329809a2a178ce79
Telion <www.neon@mail.ru>
2023-02-07 19:59:08 +0500

    Решил задачу №1

---
+++ src/main/java/site/telion/skyproworkout/tasks/task1/Task1.java
@@ -0,0 +1,28 @@
+package site.telion.skyproworkout.tasks.task1;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Task1 {
+    public static final Logger logger = LoggerFactory.getLogger(Task1.class);
+
+    public static void main(String[] args) {
+        List<String> names = new ArrayList<>(Arrays.asList("Никита", "Вася", "Оля", "Катя", "Ира"));
+        List<String> result = getFormattedNames(names);
+    }
+
+    public static List<String> getFormattedNames(List<String> names) {
+        logger.info("names = " + names);
+        List<String> formattedList = names.stream()
+                .map(name -> name.substring(1))
+                .sorted()
+                .collect(Collectors.toList());
+        logger.info("formattedList = " + formattedList);
+        return formattedList;
+    }
+}
