cddf33ed4496f9fe97f0468280af001001c8d616
Telion <www.neon@mail.ru>
2023-02-08 09:38:56 +0500

    таск1. изменил уровень логирования

--- src/main/java/site/telion/skyproworkout/SkyProWorkoutApplication.java
+++ src/main/java/site/telion/skyproworkout/SkyProWorkoutApplication.java
@@ -1,5 +1,7 @@
 package site.telion.skyproworkout;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +10,8 @@ public class SkyProWorkoutApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SkyProWorkoutApplication.class, args);
+        Logger logger = LoggerFactory.getLogger(SkyProWorkoutApplication.class);
+        logger.info("Приложение запущено");
     }
 
 }
