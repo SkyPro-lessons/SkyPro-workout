1fcfcfc5c864549f5648aea8410cb10aec0d75d7
Telion <www.neon@mail.ru>
2023-02-09 10:18:11 +0500

    Решил задачу №3

---
+++ src/main/java/site/telion/skyproworkout/tasks/task3/Task3.java
@@ -0,0 +1,25 @@
+package site.telion.skyproworkout.tasks.task3;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Task3 {
+    private static final Logger logger = LoggerFactory.getLogger(Task3.class);
+
+    public static void main(String[] args) {
+        logger.debug("'aga' is palindrome: " + isPalindrome("aga"));
+        logger.debug("'agata' is palindrome: " + isPalindrome("agata"));
+        logger.debug("'agga' is palindrome: " + isPalindrome("agga"));
+        logger.debug("'' is palindrome: " + isPalindrome(""));
+        logger.debug("'a' is palindrome: " + isPalindrome("a"));
+    }
+
+    private static boolean isPalindrome(String text) {
+        char[] textArray = text.toCharArray();
+        int textSize = textArray.length;
+        for (int i = 0; i < textSize/2; i++) {
+            if (textArray[i] != textArray[textSize - 1 - i]) return false;
+        }
+        return true;
+    }
+}
